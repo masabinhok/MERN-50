ba8908c084d6b5a88c549620191e77fecda4ee88
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-09-12 10:12:20 +0545

    test: updated connection string

--- 01-Todolist/server/index.js
+++ 01-Todolist/server/index.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 3001;
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/mtodo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/mtodo")
   .then(() => {
     console.log("Connected to MongoDB");
   })
